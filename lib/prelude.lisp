24677e18cbf9367d1f2c3d186ad8419dc6fcafae
SquidDev <bonzoweb@hotmail.co.uk>
2017-12-22 18:18:08 +0000

    Add exported-vars, returning a struct of exported variables
    Closes #35

--- lib/prelude.lisp
+++ lib/prelude.lisp
@@ -12,3 +12,27 @@
 (import math/numerics () :export)
 
 (import test/assert (affirm) :export)
+
+(defmacro exported-vars ()
+  "Generate a struct with all variables exported in the current module.
+
+   ### Example:
+   ```cl
+   > (let [(a 1)]
+   .   (exported-vars))
+   out = {\"a\" 1}
+   ```
+
+   ```cl :no-test
+   (define x 1)
+   (define y 2)
+   (define z 3)
+   (exported-vars) ;; { :x 1 :y 2 :z 3 }
+   ```"
+  (import compiler/resolve compiler)
+  (import compiler/nodes compiler)
+  (with (out '{})
+    (for-pairs (name var) (compiler/scope-exported)
+      (push-cdr! out name)
+      (push-cdr! out (compiler/var->symbol var)))
+    out))
